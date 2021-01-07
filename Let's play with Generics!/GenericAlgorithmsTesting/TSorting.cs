fb5a1465e894cef5826639890303a937c190148a
Konstantin <36280804+Burconst@users.noreply.github.com>
2021-01-07 18:48:03 +0300

    Update

--- Let's play with Generics!/GenericAlgorithmsTesting/TSorting.cs
+++ Let's play with Generics!/GenericAlgorithmsTesting/TSorting.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using GenericAlgorithms;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace GenericAlgorithmsTesting
 {
@@ -8,6 +9,7 @@ namespace GenericAlgorithmsTesting
     {
         
         List<(int[] Unsorted,int[] Sorted)> intArrayes = new List<(int[] Unsorted, int[] Sorted)>();
+        List<(BigInteger[] Unsorted,BigInteger[] Sorted)> bigIntegerArrayes = new List<(BigInteger[] Unsorted, BigInteger[] Sorted)>();
 
         [SetUp]
         public void Setup()
@@ -19,33 +21,62 @@ namespace GenericAlgorithmsTesting
                     (new []{ -123, 33, 0, 0, 0, 33 }, new []{  -123, 0, 0, 0, 33, 33 })
                     }
                 ); 
-
+            bigIntegerArrayes.AddRange(new []
+                    {
+                    (new []{ BigInteger.Parse("412341234909342340"),
+                             BigInteger.Parse("897987423480899321048921"),
+                             BigInteger.Parse("7979792348209348"),
+                             BigInteger.Parse("2350239840128301298402348"),
+                             BigInteger.Parse("4432424566434244634"),
+                             BigInteger.Parse("4432424566434244634")
+                         },
+                     new []{ BigInteger.Parse("7979792348209348"),
+                             BigInteger.Parse("412341234909342340"),
+                             BigInteger.Parse("4432424566434244634"),
+                             BigInteger.Parse("4432424566434244634"),
+                             BigInteger.Parse("897987423480899321048921"),
+                             BigInteger.Parse("2350239840128301298402348")
+                         })
+                    }
+                ); 
         }
 
         [Test]
         public void Sorting_BubbleSort_AreEqual()
         {
-            foreach (var arrPair in intArrayes)
+            foreach (var arrayes in intArrayes)
             {
-                Assert.AreEqual(arrPair.Sorted, Sorting.BubbleSort(arrPair.Unsorted));   
+                Assert.AreEqual(arrayes.Sorted, Sorting.BubbleSort(arrayes.Unsorted));   
+            }
+            foreach (var arrayes in bigIntegerArrayes)
+            {
+                Assert.AreEqual(arrayes.Sorted, Sorting.BubbleSort(arrayes.Unsorted));   
             }
         }
 
         [Test]
         public void Sorting_QuickSort_AreEqual() 
         {
-            foreach (var arrPair in intArrayes)
+            foreach (var arrayes in intArrayes)
             {
-                Assert.AreEqual(arrPair.Sorted, Sorting.QuickSort(arrPair.Unsorted));   
+                Assert.AreEqual(arrayes.Sorted, Sorting.QuickSort(arrayes.Unsorted));   
+            }
+            foreach (var arrayes in intArrayes)
+            {
+                Assert.AreEqual(arrayes.Sorted, Sorting.QuickSort(arrayes.Unsorted));   
             }
         }
 
         [Test]
         public void Sorting_HeapSort_AreEqual()
         {
-            foreach (var arrPair in intArrayes)
+            foreach (var arrayes in intArrayes)
             {
-                Assert.AreEqual(arrPair.Sorted, Sorting.HeapSort(arrPair.Unsorted));   
+                Assert.AreEqual(arrayes.Sorted, Sorting.HeapSort(arrayes.Unsorted));   
+            }
+            foreach (var arrayes in intArrayes)
+            {
+                Assert.AreEqual(arrayes.Sorted, Sorting.HeapSort(arrayes.Unsorted));   
             }
         }
 
--- Let's play with Generics!/GetStats/Program.cs
+++ Let's play with Generics!/GetStats/Program.cs
@@ -1,12 +1,99 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+using GenericAlgorithms;
 
 namespace GetStats
 {
     class Program
     {
+        static string generateNumber(uint digitsCount) 
+        {
+            if(digitsCount == 0) return "";
+            Random random = new Random(); 
+            StringBuilder numberBuilder = new StringBuilder();
+            numberBuilder.Append(random.Next(1,9).ToString());
+            for(uint i = 0; i < digitsCount-1;i++) 
+            {
+              numberBuilder.Append(random.Next(0,9).ToString());
+            }
+            return numberBuilder.ToString();
+        }
+
+        static T[] generateNumbersArray<T>(uint numbersCount, uint digitsCount, Func<string, T> converter)
+        {
+            var numbers = new List<T>();
+            for(uint i = 0; i < numbersCount; i++)
+            {
+                numbers.Add(converter(generateNumber(digitsCount)));
+            }
+            return numbers.ToArray();
+        }
+
+        static void collectSortingStats<T>(int iterations, (uint numbersCount,uint digitsCount)[] arrayInfo, Func<string, T> converter, string outputFileName) where T : IComparable
+        {
+            foreach(var info in arrayInfo) 
+            {
+                Console.WriteLine("("+ info.numbersCount+","+ info.digitsCount+")");
+                var numbers = generateNumbersArray<T>(info.numbersCount, info.digitsCount, converter);
+                var stopwatch = new Stopwatch();
+
+                Console.Write("--%");
+                using(StreamWriter sw = new StreamWriter(outputFileName+"("+ info.numbersCount+","+ info.digitsCount+")")) 
+                {
+                    sw.WriteLine("BubbleSort"+","+"HeapSort"+","+"QuickSort");
+                    for(int i = 0; i < iterations; i++) 
+                    {
+                        sw.WriteLine(
+                            getMethodStats(numbers, x => Sorting.BubbleSort(x), stopwatch)+","
+                            //getMethodStats(numbers, x => Sorting.HeapSort(x), stopwatch)+","
+                            //getMethodStats(numbers, x => Sorting.QuickSort(x), stopwatch)
+                        );
+                        if(((int)100*(i+1)/iterations) < 10)
+                        {
+                            Console.Write("\b\b\b"+" "+((int)100*(i+1)/iterations)+"%");
+                            continue;
+                        }
+                        Console.Write("\b\b\b"+((int)100*(i+1)/iterations)+"%");
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static string getMethodStats<T>(T[] numbers, Func<T[],T[]> sortingMethod, Stopwatch stopwatch) 
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+            sortingMethod(numbers);
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds.ToString();
+        }
+
+
+        static void collectStats(int iterations, uint numbersCount) 
+        {
+            Console.WriteLine("int");
+            collectSortingStats<int>(iterations, new (uint,uint)[]{(numbersCount,9)}, x => Convert.ToInt32(x), "intResult");
+            
+            Console.WriteLine("long");
+            collectSortingStats<long>(iterations, new (uint,uint)[]{(numbersCount,18)}, x => Convert.ToInt64(x), "longResult");
+
+            Console.WriteLine("BigInteger");
+            collectSortingStats<BigInteger>(iterations, new (uint,uint)[]{(numbersCount,9),(numbersCount,18), (numbersCount,27), (numbersCount, 36)},
+                                              x => BigInteger.Parse(x), "BigIntegerResult");
+            
+            Console.WriteLine("string");
+            collectSortingStats<string>(iterations, new (uint,uint)[]{(numbersCount,9),(numbersCount,18), (numbersCount,27), (numbersCount, 36)},
+                                          x => x, "stringResult");
+        }
+
         static void Main(string[] args)
         {
-            
+            collectStats(10,10000);
         }
     }
 }
